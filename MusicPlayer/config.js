6a0fdfc27e5a970db758bb410121262d9c1d3ae2
itmanlxd <itmanlxd@gmail.com>
2017-11-05 14:01:54 +0800

    Update config.js

--- MusicPlayer/config.js
+++ MusicPlayer/config.js
@@ -1,6 +1,6 @@
 var CONFIG = {
 
-songsUrl: 'itmanlxd.github.io/MusicPlayer/res/songs.json'
+songsUrl: 'https://itmanlxd.github.io/MusicPlayer/res/songs.json'
 //songsUrl: 'http://og11a17b0.bkt.clouddn.com/songs.json'
 
 };
